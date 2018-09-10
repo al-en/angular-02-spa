ff69534ce44d546d45ecda6cb5e0a316a5628b74
Almendra Estrada ♫ <al.es.na.04@gmail.com>
2018-09-10 01:16:12 -0500

    Enrutando con clase Router

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HeroesComponent implements OnInit {
+  heroes:Heroe[];
 
-  constructor() { }
+  constructor(private _heroesService:HeroesService,
+              private router:Router
+    ) {
+    // console.log("constructor Heroes");
+   }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  // Para redireccionar a la pantalla de Heroe (./components/heroe/heroe.component -> HeroeComponent)
+  verHeroe(idx:number){
+    this.router.navigate(['/heroe',idx]);
   }
 
 }
+
+
